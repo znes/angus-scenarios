4493644a7bb3a0a21b10652d71f950dafb743137
smn <simon.hilpert@uni-flensburg.de>
2019-07-22 12:12:48 +0200

    Update analysis

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -3,6 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datapackage as dp
 import plotly.io as pio
+import plotly.offline as offline
+
+from plots import (hourly_plot, stacked_plot, price_line_plot,
+                   price_scatter_plot, merit_order_plot)
 
 country = 'DE'
 
@@ -11,33 +15,33 @@ sorted = {}
 unsorted = {}
 for r in os.listdir("results"):
     path = os.path.join("results", r, "output", "shadow_prices.csv")
-    s = pd.read_csv(path, index_col=[0], parse_dates=True)["DE-electricity"]
-    sorted[r] = s.sort_values().values
-    unsorted[r] = s.values
-
-#pd.DataFrame(sorted).to_csv("results/shadow_prices_sorted.csv")
-#pd.DataFrame(unsorted).to_csv("results/shadow_prices_unsorted.csv")
+    sprices = pd.read_csv(path, index_col=[0], parse_dates=True)[country + "-electricity"]
+    sorted[r] = sprices.sort_values().values
+    unsorted[r] = sprices.values
 
 
 # residual load and more
 renewables = ['wind-onshore', 'wind-offshore', "solar-pv", "hydro-ror"]
 timestamps = {}
-if country == 'DE':
-    rload = {}
-    marginal_cost = {}
-    shadow_prices = {}
-    storages = {}
-    prices = {}
-    for r in os.listdir("results"):
-        path = os.path.join("results", r, "output", country + "-electricity.csv")
-        country_electricity_df = pd.read_csv(path, index_col=[0], parse_dates=True)
-        country_electricity_df['rload'] = (
-            country_electricity_df[("-").join([country, 'electricity-load'])] -
-            country_electricity_df[[("-").join([country, i]) for i in renewables]].sum(axis=1)
-        )
 
-        rload[r] = country_electricity_df['rload'].values
-        timestamps[r] = country_electricity_df.index
+marginal_cost = {}
+shadow_prices = {}
+storages = {}
+prices = {}
+rload = {}
+for r in os.listdir("results"):
+
+    path = os.path.join("results", r, "output", country + "-electricity.csv")
+    country_electricity_df = pd.read_csv(path, index_col=[0], parse_dates=True)
+    country_electricity_df['rload'] = (
+        country_electricity_df[("-").join([country, 'electricity-load'])] -
+        country_electricity_df[[("-").join([country, i]) for i in renewables]].sum(axis=1)
+    )
+
+    rload[r] = country_electricity_df['rload'].values
+    timestamps[r] = country_electricity_df.index
+
+    if country == 'DE':
 
         path = os.path.join("results", r, "input", "datapackage.json")
         input_datapackage = dp.Package(path)
@@ -59,7 +63,7 @@ if country == 'DE':
         storages[r] = storages[r].rename(
             columns={0:'storage_dispatch', 'DE-electricity': 'shadow_price'})
 
-        prices[r] =[]
+        prices[r] = []
         for c in country_electricity_df.iterrows():
             timestamp = c[0]
             energy = c[1]
@@ -82,36 +86,48 @@ if country == 'DE':
                              marginal_cost = df_marginal_cost,
                              shadow_price = shadow_prices[r][timestamp])
             prices[r].append(temp_dict)
-        prices[r] = pd.DataFrame(prices[r]).set_index('timestamp')
 
-from plots import hourly_plot, stacked_plot, price_line_plot, price_scatter_plot, merit_order_plot
-import plotly.offline as offline
+prices = {r: pd.DataFrame(prices[r]).set_index('timestamp') for r in prices}
 
-if not os.path.exists('plots'):
-    os.makedirs('plots')
-#
-if True:
-    for s in os.listdir('results'):
-        offline.plot(
-            stacked_plot(s), filename=os.path.join('plots', s + '-capacities'))
-        offline.plot(
-            hourly_plot(s, 'DE'), filename=os.path.join('plots', s + '-dispatch'))
+results = [r for r in os.listdir('results') if 'plots' not in r]
 
-#offline.plot(
-#          price_line_plot(os.listdir('results'), s.index, unsorted),
-#          filename=os.path.join('plots', 'shadow_prices'))
+if not os.path.exists('results/plots'):
+    os.makedirs('results/plots')
+
+# plot for all scenarios
+offline.plot(
+    price_line_plot(results, sprices.index, unsorted, country),
+    filename=os.path.join('results', 'plots', 'shadow-prices.html'),
+    auto_open=False)
+
+offline.plot(
+    price_scatter_plot(
+        results, rload, unsorted, timestamps, country),
+    filename=os.path.join(
+        'results', 'plots', 'shadow_prices-vs-residual_load.html'),
+    auto_open=False)
 
 
-# offline.plot(
-#           price_scatter_plot(os.listdir('results'), rload, unsorted, timestamps),
-#           filename=os.path.join('plots', 'shadow_prices'))
-
-for s in os.listdir('results'):
+for s in results:
+    scenario_plots = os.path.join('results', 'plots', s)
+    if not os.path.exists(scenario_plots):
+        os.makedirs(scenario_plots)
     offline.plot(
-        merit_order_plot(s, prices, storages),
-        filename = os.path.join('plots', 'merit_order_'+s+'.html'),
-        auto_open = False)
+         stacked_plot(s),
+         filename=os.path.join(scenario_plots, s + '-capacities.html'),
+         auto_open=False)
+    offline.plot(
+         hourly_plot(s, 'DE'),
+         filename=os.path.join(scenario_plots, s + '-dispatch.html'),
+         auto_open=False)
 
-    pio.write_image(
-        merit_order_plot(s, prices, storages),
-        os.path.join('plots', 'merit_order_'+s+'.pdf'))
+    if country == 'DE':
+        offline.plot(
+            merit_order_plot(s, prices, storages),
+            filename = os.path.join(
+                scenario_plots, 'merit-order-' + s + '.html'),
+            auto_open = False)
+
+        # pio.write_image(
+        #     merit_order_plot(s, prices, storages),
+        #     os.path.join(scenario_plots, 'merit_order_' + s +'.pdf'))
