a977dd181a840385ab105525ee1cc8cba3257065
simnh <simon.hilpert@uni-flensburg.de>
2020-02-13 15:12:16 +0100

    Fix sensitivities

--- scripts/generate_sensitivities.py
+++ scripts/generate_sensitivities.py
@@ -9,7 +9,7 @@ for dir in os.listdir("scenarios"):
     scenario = toml.load(os.path.join("scenarios", dir))
     for f in flex:
         filename = dir.split(".")[0] + "-flex" + str(f) + ".toml"
-        scenario["scenario"]["decentral_flex_share"] = f  / 100
+        scenario["scenario"]["decentral_heat-flex-share"] = f  / 100
         scenario["name"] = filename.replace(".toml", "")
         scenario["title"] = filename.replace(".toml", "")
         toml_string = toml.dumps(scenario)
