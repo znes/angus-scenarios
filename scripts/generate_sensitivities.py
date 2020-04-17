1b653bf3b9b6f069d41c3e209b298c5806e5e397
simnh <simon.hilpert@uni-flensburg.de>
2020-04-17 12:06:48 +0200

    Remove scenarios and update sensitivities

--- scripts/generate_sensitivities.py
+++ scripts/generate_sensitivities.py
@@ -1,29 +1,28 @@
 import os
 import toml
 
-heat = {"2050REF-GS.toml": [-30, -20, -10, 0, 10, 20, 30],
-        "2050REF.toml": [-30, -20, -10, 0, 10, 20, 30]}
-total_demand = {"2050REF-GS.toml": 184000, "2050REF.toml": 284000}
 
-for dir in os.listdir("scenarios"):
-    scenario = toml.load(os.path.join("scenarios", dir))
-    if dir in heat.keys():
-        for k,v in heat.items():
-            for vv in v:
-                filename = dir.split(".")[0] + "-load-" + str(vv) + ".toml"
-                scenario["sensitivities"].update({"heat": {"DE-decentral_heat-load": total_demand[k] * (1 + vv/100)}})
-                scenario["scenario"]["decentral_heat-flex-share"] = 1
-                scenario["name"] = filename.replace(".toml", "")
-                scenario["title"] = filename.replace(".toml", "")
-                toml_string = toml.dumps(scenario)
-                with open(os.path.join("scenarios", filename), mode='w') as w:
-                    w.writelines(toml_string)
 
-biomass_share = [0, 20, 40, 60, 80, 100]
+demand = {"2030DG.toml": "2030GS" ,"2040DG.toml": "2040GS", "2050REF.toml": "2050GS"}
+
+
+for k,v  in demand.items():
+    print(k,v)
+    scenario = toml.load(os.path.join("scenarios", k))
+    filename = k.split(".")[0] + "-GS.toml"
+    scenario["scenario"]["DE_heat_system"] = v
+    scenario["scenario"]["decentral_heat-flex-share"] = 1
+    scenario["name"] = filename.replace(".toml", "")
+    scenario["title"] = filename.replace(".toml", "")
+    toml_string = toml.dumps(scenario)
+    with open(os.path.join("scenarios", filename), mode='w') as w:
+        w.writelines(toml_string)
+
+biomass_share = [10, 30, 50, 70, 90]
 for dir in os.listdir("scenarios"):
     scenario = toml.load(os.path.join("scenarios", dir))
     for b in biomass_share:
-        if "2050REF" in dir:
+        if "2050REF.toml" in dir:
             if not "-load" in dir:
                 filename = dir.split(".")[0] + "-bio-" + str(b) + ".toml"
                 scenario["sensitivities"].update({"biomass": {"DE-biomass-commodity": b / 100}})
